9725a17ade133922e6f27bba6dc429030a3711e1
ivanroscuenca <ivanros@protonmail.com>
2023-08-08 18:52:40 +0200

    video120

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,11 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
   searchByCapital(term: string): void {
-    console.log({ term });
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
 }
