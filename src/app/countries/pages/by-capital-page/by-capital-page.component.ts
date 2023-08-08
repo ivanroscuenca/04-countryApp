a2206096fc67f638bf0c984b559e1cf4f2fbb963
ivanroscuenca <ivanros@protonmail.com>
2023-08-08 12:11:59 +0200

    video119

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+})
+export class ByCapitalPageComponent {
+  searchByCapital(term: string): void {
+    console.log({ term });
+  }
+}
