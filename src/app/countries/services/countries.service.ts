a864d054207a5748436815b3b4955dbabe21fb68
ivanroscuenca <ivanros@protonmail.com>
2023-08-10 11:19:39 +0200

    video123

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -14,4 +14,16 @@ export class CountriesService {
       .get<Country[]>(`${this.apiUrl}/capital/${term}`)
       .pipe(catchError((error) => of([])));
   }
+
+  searchCountry(term: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/name/${term}`)
+      .pipe(catchError((error) => of([])));
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/region/${term}`)
+      .pipe(catchError((error) => of([])));
+  }
 }
